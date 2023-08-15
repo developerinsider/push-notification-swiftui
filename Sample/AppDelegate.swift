57a8697a267a58cb658c12d4da0f566c9dabf883
Vineet Choudhary <vineetchoudhary@live.in>
2023-08-15 14:24:46 +0530

    Request push notification from app delegate

--- Sample/AppDelegate.swift
+++ Sample/AppDelegate.swift
@@ -10,8 +10,26 @@ import UIKit
 import OSLog
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+		// request push notification authorization
+		UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { allowed, error in
+			if allowed {
+				// register for remote push notification
+				DispatchQueue.main.async {
+					UIApplication.shared.registerForRemoteNotifications()
+				}
+				Log.shared.info("Push notification allowed by user")
+			} else {
+				Log.shared.debug("Error while requesting push notification permission. Error \(error)")
+			}
+		}
+		return true
+	}
+
+
 	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-		let tokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+		let tokenString = deviceToken.reduce("", {$0 + String(format: "%02x", $1)})
 		Log.shared.info("Device push notification token - \(tokenString)")
 	}
 
