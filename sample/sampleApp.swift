9777c0f90ae19025886b72d57e0ece05648b2014
Vineet Choudhary <vineetchoudhary@live.in>
2023-08-15 10:19:27 +0530

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Vineet Choudhary on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
